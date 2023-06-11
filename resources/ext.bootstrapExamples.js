8f844bf503efb98520d415b4f68a28dd7359d706
Morne Alberts <morne@malberts.net>
2023-06-11 09:45:02 +0200

    Change script dependency waiting

--- resources/ext.bootstrapExamples.js
+++ resources/ext.bootstrapExamples.js
@@ -7,18 +7,22 @@
 	mw.bootstrapExamples = {
 	};
 
-	// Get Bootstrap library version.
-	var version = $.fn.tooltip.Constructor.VERSION;
-	if ( version === undefined ) {
-		version = 'not found';
-	}
-	document.getElementById( 'bootstrap-version' ).textContent = version;
+	mw.hook( 'wikipage.content' ).add( () => {
+		mw.loader.using( 'ext.bootstrap.scripts', () => {
+			// Get Bootstrap library version.
+			var version = $.fn.tooltip.Constructor.VERSION;
+			if ( version === undefined ) {
+				version = 'not found';
+			}
+			document.getElementById( 'bootstrap-version' ).textContent = version;
 
-	$( 'a[href="#"]' ).click(function ( event ) {
-		event.preventDefault();
-	});
+			$( 'a[href="#"]' ).click( function ( event ) {
+				event.preventDefault();
+			} );
 
-	$( '.bs-component [data-bs-toggle="popover"]' ).popover();
-	$( '.bs-component [data-bs-toggle="tooltip"]' ).tooltip();
+			$( '.bs-component [data-bs-toggle="popover"]' ).popover();
+			$( '.bs-component [data-bs-toggle="tooltip"]' ).tooltip();
+		} );
+	} );
 
 }() );
